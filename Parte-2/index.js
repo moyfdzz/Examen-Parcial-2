d10404db7444eb9fe53cad0ac265ac7e11644541
Moisés Fernández <moy-881@hotmail.com>
2020-01-29 08:50:29 -0600

    Finished part 2 and 3

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -8,7 +8,89 @@ let app = express();
 
 let server;
 
-/* Tu código va aquí */
+let bookmarks = [
+{
+	id: "0da8b32a-aaff-434f-1bdd-77985e285ba3",
+	titulo: "Google",
+	descripcion: "El sabiondo",
+	url: "https://www.google.com"
+},
+{
+	id: "f38c7e59-55dd-433b-2fff-c350bb8f9ce5",
+	titulo: "Facebook",
+	descripcion: "La mega red social",
+	url: "https://www.facebook.com"
+},
+{
+	id: "70f10dcb-d7ca-4476-93fe-a2ad6a4994a7",
+	titulo: "YouTube",
+	descripcion: "Videos para todos",
+	url: "https://www.youtube.com"
+},
+{
+	id: "d6362ed6-5825-4c45-a7f4-0d2bbd005ebf",
+	titulo: "Wikipedia",
+	descripcion: "La tia wiki",
+	url: "https://www.wikipedia.org"
+}];
+
+app.put('/api/bookmarks/:id', jsonParser, (req,res) => {
+	let idCuerpo = req.body.id;
+	let idParametro = req.params.id;
+
+	if (idCuerpo == undefined) {
+		res.statusMessage = 'Error 406: El id no fue enviado en el cuerpo.';
+
+		return res.status(406).send();
+	}
+
+	if (idCuerpo !== idParametro) {
+		res.statusMessage = 'Error 409: El id del cuerpo no coincide con el id enviado como parámetro.';
+
+		return res.status(409).send();
+	}
+
+	let titulo = req.body.titulo;
+	let descripcion = req.body.descripcion;
+	let url = req.body.url;
+
+	if (titulo === '' && descripcion === '' && url === '') {
+		res.statusMessage = 'Ingrese todos los datos para actualizar un bookmark existente.';
+
+		return res.status(406).send();
+	}
+
+	let foundBookmark = false;
+	let updatedBookmark;
+
+	bookmarks.forEach((bookmark) => {
+		if (bookmark.id.toString() === idParametro) {
+			foundBookmark = true;
+
+			if (titulo !== '') {
+				bookmark.titulo = titulo;
+			}
+
+			if (descripcion !== '') {
+				bookmark.descripcion = descripcion;
+			}
+
+			if (url !== '') {
+				bookmark.url = url;
+			}
+
+			updatedBookmark = bookmark;
+		}
+	});
+
+	if (!foundBookmark) {
+        res.statusMessage = 'No existe un bookmark con el título ingresado.';
+
+        return res.status(404).send();
+    }
+
+    return res.status(202).send(updatedBookmark);
+});
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -3,8 +3,27 @@ let uuid = require( 'uuid' );
 
 mongoose.Promise = global.Promise;
 
-/* Tu código va aquí */
+let bookmarks = mongoose.Schema({
+    id: uuid.v4(),
+    titulo: String,
+    descripcion: String,
+    url: String
+});
+
+let bookmark = ('bookmarksdb', bookmarks);
+
+let bookmarkController = {
+    update: function(id, updatedBookmark) {
+        return bookmark.findOneAndUpdate(id, updatedBookmark)
+            .then(ub => {
+                return ub;
+            })
+            .catch(error => {
+                throw Error(error);
+            })
+    }
+}
 
 module.exports = {
-    
+    bookmarkController
 };
